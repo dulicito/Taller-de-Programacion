f87b2edee6f407e6842f057f1ddd98d0eb80e289
Paredes Dulce <pv.sofiadulce@gmail.com>
2024-10-03 10:04:31 -0300

    Update ejercicio1.java

--- Modulo Objetos/Practicas/Practica 3/ejercicio1.java
+++ Modulo Objetos/Practicas/Practica 3/ejercicio1.java
@@ -1,8 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ENUNCIADO:
+1-A- Definir una clase para representar triángulos. Un triángulo se caracteriza por el
+tamaño de sus 3 lados (double), el color de relleno (String) y el color de línea (String).
+Provea un constructor que reciba todos los datos necesarios para iniciar el objeto.
+Provea métodos para:
+- Devolver/modificar el valor de cada uno de sus atributos (métodos get y set)
+- Calcular el perímetro y devolverlo (método calcularPerimetro)
+- Calcular el área y devolverla (método calcularArea)
+B- Realizar un programa que instancie un triángulo, le cargue información leída desde
+teclado e informe en consola el perímetro y el área.
+NOTA: Calcular el área con la fórmula Área = s(s − a)(s − b)(s − c) , donde a, b y c
+son los lados y s = . La función raíz cuadrada es Math.sqrt(#) a+b+c
+    
 package tema3;
 
 import PaqueteLectura.Lector;
