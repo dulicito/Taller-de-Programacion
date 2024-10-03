c4c2375feded1ab3b6ed71576ad190c7de2c5e4f
Paredes Dulce <pv.sofiadulce@gmail.com>
2024-10-03 10:07:41 -0300

    Update ejercicio3.java

--- Modulo Objetos/Practicas/Practica 4/ejercicio3.java
+++ Modulo Objetos/Practicas/Practica 4/ejercicio3.java
@@ -1,8 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ENUNCIADO:
+3-A- Implemente las clases para el siguiente problema. Una garita de seguridad quiere
+identificar los distintos tipos de personas que entran a un barrio cerrado. Al barrio pueden
+entrar: personas, que se caracterizan por nombre, DNI y edad; y trabajadores, estos son
+personas que se caracterizan además por la tarea realizada en el predio.
+Implemente constructores, getters y setters para las clases. Además tanto las personas
+como los trabajadores deben responder al mensaje toString siguiendo el formato:
+
+▪ Personas “Mi nombre es Mauro, mi DNI es 11203737 y tengo 70 años”
+▪ Trabajadores “Mi nombre es Mauro, mi DNI es 11203737 y tengo 70 años. Soy
+jardinero.”
+    
+B- Realice un programa que instancie una persona y un trabajador y muestre la
+representación de cada uno en consola.
+NOTA: Reutilice la clase Persona (carpeta tema2).
+
+    
 package tema4;
 
 import PaqueteLectura.GeneradorAleatorio;
